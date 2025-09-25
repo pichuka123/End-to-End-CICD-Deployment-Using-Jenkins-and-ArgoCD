b5a02a8a4d5da0827ee1ad8acd57a1a9dfb8a4ad
Prince <prince.charu7@gmail.com>
2025-09-25 23:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello from DevOps World!!!"
+    return "Hello from DevOps World!!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
